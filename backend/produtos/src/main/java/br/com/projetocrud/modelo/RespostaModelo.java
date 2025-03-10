1dce4125688c9fbd9c2e5b4e5d906fc01c541cf4
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-03-10 08:13:51 -0300

    Criação da Classe RespostaModelo.

---
+++ backend/produtos/src/main/java/br/com/projetocrud/modelo/RespostaModelo.java
@@ -0,0 +1,13 @@
+package br.com.projetocrud.modelo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Setter
+@Getter
+public class RespostaModelo {
+  private String resposta;
+}
