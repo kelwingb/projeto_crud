5ab87bf923d8ad0c146cc8e9cd115c6a65e1a021
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-03-10 07:52:28 -0300

    Criação método listar() do serviço.

--- backend/produtos/src/main/java/br/com/projetocrud/repositorio/ProdutoRepositorio.java
+++ backend/produtos/src/main/java/br/com/projetocrud/repositorio/ProdutoRepositorio.java
@@ -1,9 +1,11 @@
 package br.com.projetocrud.repositorio;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.projetocrud.modelo.ProdutoModelo;
 
+@Repository
 public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
   
 }
--- backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
@@ -1,5 +1,17 @@
 package br.com.projetocrud.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projetocrud.modelo.ProdutoModelo;
+import br.com.projetocrud.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
-  
+  @Autowired
+  private ProdutoRepositorio pr;
+
+  public Iterable<ProdutoModelo> listar(){
+    return pr.findAll();
+  }
 }
