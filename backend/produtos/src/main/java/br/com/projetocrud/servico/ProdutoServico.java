3255e8884604420481e2b0478b275613ff7debe8
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-03-11 14:17:54 -0300

    Serviço para cadastrar produtos.

--- backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
@@ -1,9 +1,12 @@
 package br.com.projetocrud.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.projetocrud.modelo.ProdutoModelo;
+import br.com.projetocrud.modelo.RespostaModelo;
 import br.com.projetocrud.repositorio.ProdutoRepositorio;
 
 @Service
@@ -11,7 +14,22 @@ public class ProdutoServico {
   @Autowired
   private ProdutoRepositorio pr;
 
+  @Autowired
+  private RespostaModelo rm;
+
   public Iterable<ProdutoModelo> listar(){
     return pr.findAll();
+  }
+
+  public ResponseEntity <?> cadastrar(ProdutoModelo pm){
+    if (pm.getNome().equals("")) {
+      rm.setResposta("O nome do Produto é obrigatório!");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+    } else if (pm.getMarca().equals("")) {
+      rm.setResposta("A marca do produto é obrigatório!");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST); 
+    } else {
+      return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+    }
   }
 }
