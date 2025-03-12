3e4c57a49207fc953b5c7706e8becb43c741f394
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-03-12 10:56:34 -0300

    Rota para cadastrar produtos.

--- backend/produtos/src/main/java/br/com/projetocrud/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/projetocrud/controle/ProdutoControle.java
@@ -1,7 +1,11 @@
 package br.com.projetocrud.controle;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projetocrud.modelo.ProdutoModelo;
@@ -12,6 +16,10 @@ public class ProdutoControle {
 
   @Autowired
   private ProdutoServico ps;
+  @PostMapping("/cadastrar")
+  public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+    return ps.cadastrar(pm);
+  }
 
   @GetMapping("/listar")
   public Iterable<ProdutoModelo> listar(){
