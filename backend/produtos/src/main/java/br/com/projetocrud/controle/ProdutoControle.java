9c3bb00a1b480f97a1b9e3937c2b8019a35b69b1
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-04-01 07:27:58 -0300

    CORS resolvido!

--- backend/produtos/src/main/java/br/com/projetocrud/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/projetocrud/controle/ProdutoControle.java
@@ -2,6 +2,7 @@ package br.com.projetocrud.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.projetocrud.modelo.RespostaModelo;
 import br.com.projetocrud.servico.ProdutoServico;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
 
   @Autowired
