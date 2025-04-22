8ea53af3040cab82cafca50030e3689b70222898
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-04-22 20:10:22 -0300

    Estilização e visibilidade do forms e botões.

--- frontend/front/src/Formulario.js
+++ frontend/front/src/Formulario.js
@@ -1,24 +1,29 @@
-function Formulario(botao) {
-  return (
-    <form>
-      <h1>Formulário</h1>
+  function Formulario({ botao }) {
+    return (
+      <form>
+        <h1>Formulario</h1>
+        <input type="text" placeholder="Nome"></input>
+        <input type="text" placeholder="Marca"></input>
 
-      <input className="form-control" type="text" placeholder="Nome" />
-      <input className="form-control" type="text" placeholder="Marca" />
+        <input type="button" value="Cadastrar" />
+        <input type="button" value="Cancelar" />
+        <input type="button" value="Alterar" />
+        <input type="button" value="Remover" />
 
-      {
-        botao
-          ?
-          <input className="btn btn-success" type="button" value="Cadastrar" />
-          :
-          <div>
-            <input className="btn btn-dark" type="button" value="Alterar" />
-            <input className="btn btn-danger" type="button" value="Remover" />
-            <input className="btn btn-warning" type="button" value="Cancelar" />
-          </div>
-      }
-    </form>
-  )
-}
-
-export default Formulario;
+        <input className="form-control" type="text" placeholder="Nome"></input>
+        <input className="form-control" type="text" placeholder="Marca"></input>
+        {
+          botao
+            ?
+            <input className="btn btn-success" type="button" value="Cadastrar" />
+            :
+            <div>
+              <input className="btn btn-warning" type="button" value="Cancelar" />
+              <input className="btn btn-primary" type="button" value="Alterar" />
+              <input className="btn btn-danger" type="button" value="Remover" />
+            </div>
+        }
+      </form>
+    )
+  }
+  export default Formulario;
