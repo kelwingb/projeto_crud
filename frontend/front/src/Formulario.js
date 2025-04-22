8f654768a61a3769a03c737bd0194175d630562f
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-04-22 19:07:44 -0300

    Bootstrap instalado, componentes de formulário e tabela criados

---
+++ frontend/front/src/Formulario.js
@@ -0,0 +1,24 @@
+function Formulario(botao) {
+  return (
+    <form>
+      <h1>Formulário</h1>
+
+      <input className="form-control" type="text" placeholder="Nome" />
+      <input className="form-control" type="text" placeholder="Marca" />
+
+      {
+        botao
+          ?
+          <input className="btn btn-success" type="button" value="Cadastrar" />
+          :
+          <div>
+            <input className="btn btn-dark" type="button" value="Alterar" />
+            <input className="btn btn-danger" type="button" value="Remover" />
+            <input className="btn btn-warning" type="button" value="Cancelar" />
+          </div>
+      }
+    </form>
+  )
+}
+
+export default Formulario;
