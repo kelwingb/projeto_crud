7171b965cd1a8d5c08f212a3b2f8e389c22ec172
Kelwin Gabriel <kelwimgabriel@gmail.com>
2025-04-22 20:31:00 -0300

    Alterações no Form.

--- frontend/front/src/Formulario.js
+++ frontend/front/src/Formulario.js
@@ -2,14 +2,7 @@
     return (
       <form>
         <h1>Formulario</h1>
-        <input type="text" placeholder="Nome"></input>
-        <input type="text" placeholder="Marca"></input>
-
-        <input type="button" value="Cadastrar" />
-        <input type="button" value="Cancelar" />
-        <input type="button" value="Alterar" />
-        <input type="button" value="Remover" />
-
+  
         <input className="form-control" type="text" placeholder="Nome"></input>
         <input className="form-control" type="text" placeholder="Marca"></input>
         {
@@ -18,6 +11,7 @@
             <input className="btn btn-success" type="button" value="Cadastrar" />
             :
             <div>
+              <input className="btn btn-success" type="button" value="Cadastrar" />
               <input className="btn btn-warning" type="button" value="Cancelar" />
               <input className="btn btn-primary" type="button" value="Alterar" />
               <input className="btn btn-danger" type="button" value="Remover" />
